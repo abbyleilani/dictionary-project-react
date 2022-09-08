798d6d122346a5c90fbba5ebb728e3c5cf4e8472
abbyleilani <abbyleilanibaker@gmail.com>
2022-09-08 12:17:21 -0700

    Added Synonyms
    created synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,23 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  // console.log(props.meaning);
+  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>;
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition}
-              <br /> <em> {definition.example} </em>
-            </p>
+            <div>
+              {" "}
+              <strong>Definitions:</strong>
+              {definition.definition} <br />
+              <strong>example:</strong>
+              <em> {definition.example}</em>
+              <br /> <strong>synonyms:</strong>
+              <Synonyms synonyms={definition.synonyms} />{" "}
+            </div>
           </div>
         );
       })}
