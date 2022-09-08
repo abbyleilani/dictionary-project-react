41e90c85947b9de85c8fc00e638c0efd5e9815c0
abbyleilani <abbyleilanibaker@gmail.com>
2022-09-07 22:58:12 -0700

    Rendered the definitions
    created Meaning and Results components

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyWord, setKeyWord] = useState("");
+  const [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    // console.log(response.data[0])
+    // console.log(response.data[0].meanings[0].definitions[0].definition);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -29,6 +33,7 @@ export default function Dictionary() {
           <input type="submit" />
         </form>
       </div>
+      <Results results={results} />
     </div>
   );
 }
