6f415febc3bccf761a21fcf839e6eeefedb8ce74
abbyleilani <abbyleilanibaker@gmail.com>
2022-09-15 10:41:28 -0700

    added an if statement to function Meaning
    updated line 19  from to props.meaning.synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,25 +2,29 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
-  return (
-    <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>;
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <div>
-              {" "}
-              <strong>Definitions:</strong>
-              {definition.definition} <br />
-              <strong>example:</strong>
-              <em> {definition.example}</em>
-              <br /> <strong>synonyms:</strong>
-              <Synonyms synonyms={definition.synonyms} />{" "}
+  //  console.log(props.meaning);
+  if (props.meaning) {
+    return (
+      <div className="Meaning">
+        <h3>{props.meaning.partOfSpeech}</h3>;
+        {props.meaning.definitions.map(function (definition, index) {
+          return (
+            <div key={index}>
+              <div>
+                {" "}
+                <strong>Definitions:</strong>
+                {definition.definition} <br />
+                <strong>example:</strong>
+                <em> {definition.example}</em>
+                <br /> <strong>synonyms:</strong>
+                <Synonyms synonyms={props.meaning.synonyms} />{" "}
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
