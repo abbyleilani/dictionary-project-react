34f3999628cf79a1abe616f1af6ed7c7c6d77407
abbyleilani <abbyleilanibaker@gmail.com>
2022-09-30 13:10:10 -0700

    installed pexels, incorporated API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyWord, setKeyWord] = useState("sunset");
   const [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     // console.log(response.data[0])
     // console.log(response.data[0].meanings[0].definitions[0].definition);
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    //console.log(response);
+    setPhotos(response.data.photos);
   }
 
   function handleKeyWordChange(event) {
@@ -21,7 +28,7 @@ export default function Dictionary() {
   function search(event) {
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
   }
 
   function handleSubmit(event) {
@@ -34,6 +41,11 @@ export default function Dictionary() {
     search();
   }
 
+  let pexelsApiKey = "563492ad6f91700001000001281ec43e36c246d79e5039db049e6023";
+  let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyWord}&per_page=1`;
+  let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+  axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+
   if (loaded) {
     return (
       <div className="Dictionary">
@@ -44,6 +56,9 @@ export default function Dictionary() {
             </form>
             <div className="hint"> suggested words: alien, star, cherry...</div>
           </section>
+          <div>
+            <Photos photos={photos} />
+          </div>
           <Results results={results} />
         </div>
       </div>
